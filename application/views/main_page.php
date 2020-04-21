c57dce99df47651e3094bf4319647e5086269b4d
Dmitrii Morozov <DVMorozov74@gmail.com>
2020-04-21 12:06:50 +0500

    add v-cloak

--- application/views/main_page.php
+++ application/views/main_page.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 <div id="app">
-  <div class="header">
+  <div class="header" v-cloak>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
               aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -291,6 +291,9 @@
 <script src="/js/app.js?v=<?= filemtime(FCPATH . '/js/app.js') ?>"></script>
 
 </body>
+<style>
+    [v-cloak] {
+        display: none;
+    }
+</style>
 </html>
-
-
